9c0b511d37a7d3ad073c966a81f3fc677cb8448a
mattsuglyandsucks-sudo <mattsuglyandsucks@gmail.com>
2026-08-26 19:59:09 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="shadow-grid-v7";
+const CACHE="shadow-grid-v9";
 const CORE=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install",event=>{
